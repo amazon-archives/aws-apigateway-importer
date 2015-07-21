9a54a58d09a91a559c607a06786abbc2f640d473
Ryan Green <rpgreen@amazon.com>
2015-07-21 12:54:49 -0700

    Use default region if region settings not configured

--- src/com/amazonaws/service/apigateway/importer/config/AwsConfig.java
+++ src/com/amazonaws/service/apigateway/importer/config/AwsConfig.java
@@ -1,10 +1,15 @@
 package com.amazonaws.service.apigateway.importer.config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class AwsConfig {
+    private static final Log LOG = LogFactory.getLog(AwsConfig.class);
+
     private String region = "us-east-1";
     private String profile = "default";
 
@@ -17,20 +22,25 @@ public class AwsConfig {
     }
 
     public void load() {
-
         String home = System.getProperty("user.home");
-//        ProfilesConfigFile file = new ProfilesConfigFile(home + "/.aws/config");
 
         Properties awsConfigProperties = new Properties();
 
-        // todo: use profile
         try {
+            // todo: use default profile
             awsConfigProperties.load(new FileInputStream(home + "/.aws/config"));
         } catch (IOException e) {
             throw new RuntimeException("Could not load configuration. Please run 'aws configure'");
         }
 
-        region = awsConfigProperties.getProperty("region");
+        String region = awsConfigProperties.getProperty("region");
+
+        if (region != null) {
+            this.region = region;
+        } else {
+            LOG.warn("Could not load region configuration. Please ensure AWS CLI is configured via 'aws configure'. Will use default region of " + this.region);
+        }
+
     }
 
 }
