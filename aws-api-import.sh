d66afd53bf798beaf436790b278b0ef3a5ae5d0d
Chris Kozak <ckozak@gmail.com>
2015-08-24 15:55:56 -0500

    Making jar executable.

--- aws-api-import.sh
+++ aws-api-import.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp build/maven/aws-apigateway-swagger-importer-1.0.0-jar-with-dependencies.jar com.amazonaws.service.apigateway.importer.ApiImporterMain "$@"
+java -jar build/maven/aws-apigateway-swagger-importer-1.0.0-jar-with-dependencies.jar "$@"
