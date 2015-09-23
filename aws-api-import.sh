26a442c09ab7de8c2467f6c1af4045ab93d92ec1
Ryan Green <rpgreen@amazon.com>
2015-09-23 16:41:13 -0700

    Bump version number in the launch scripts

--- aws-api-import.sh
+++ aws-api-import.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar build/maven/aws-apigateway-swagger-importer-1.0.0-jar-with-dependencies.jar "$@"
+java -jar build/maven/aws-apigateway-swagger-importer-1.0.1-jar-with-dependencies.jar "$@"
