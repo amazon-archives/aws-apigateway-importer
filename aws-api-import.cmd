5d6d413fb65661406403ce2ef9b620686953d8c1
David Purrington <dpurrington@cimpress.com>
2015-12-02 14:36:36 -0500

    make scripts runnable from other locations

--- aws-api-import.cmd
+++ aws-api-import.cmd
@@ -1,1 +1,1 @@
-java -jar target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar  %*
+java -jar %~dp0target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar  %*
--- aws-api-import.sh
+++ aws-api-import.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+root=$(dirname "$(readlink -f "$0")")
 
-java -jar target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar "$@"
+java -jar $root/target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar "$@"
